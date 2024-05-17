bc0683767e7efa551413bdb6116d145296644d2b
Santhosh-M09 <santhoshmuthup@gmail.com>
2024-05-17 09:54:30 +0530

    update

--- Javascript/arrowfunction.js
+++ Javascript/arrowfunction.js
@@ -1,6 +1,14 @@
-// Arrow Function
-const add = (a, b) => {
-    return a + b;
-};
+const products = [
+    { id: 1, name: "Laptop", price: 1000, inStock: true },
+    { id: 2, name: "Mouse", price: 20, inStock: false },
+    { id: 3, name: "Keyboard", price: 50, inStock: true },
+    { id: 4, name: "Monitor", price: 300, inStock: false },
+    { id: 5, name: "USB Cable", price: 10, inStock: true },
+  ];
 
-console.log("Addition:", add(8, 3));
+  const inStockProducts = products.filter(product => product.inStock);
+  
+  const productNames = inStockProducts.map(product => product.name);
+  
+  console.log("In-stock Product Names:", productNames);
+  
